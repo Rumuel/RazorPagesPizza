fffcfe3a9b7f045b5d9c2606d2d34aa6025e3beb
rumuel.plasdan@hotmail.com <rumuel.plasdan@hotmail.com>
2021-10-13 11:27:02 +0100

    Add project files.

---
+++ RazorPagesPizza/Models/Pizza.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesPizza.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public PizzaSize Size { get; set; }
+        public bool IsGlutenFree { get; set; }
+
+        [Range(0.01, 9999.99)]
+        public decimal Price { get; set; }
+
+        public string GlutenFreeText(Pizza pizza)
+        {
+            if (pizza.IsGlutenFree)
+                return "Gluten Free";
+            return "Not Gluten Free";
+        }
+    }
+
+    public enum PizzaSize { Small, Medium, Large }
+
+   
+}
